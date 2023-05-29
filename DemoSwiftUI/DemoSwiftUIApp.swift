7378355cefb17ed7081c0de785706cac27e53a68
sang0913 <74355128+sang0913@users.noreply.github.com>
2023-05-30 01:11:50 +0700

    Initial Commit

---
+++ DemoSwiftUI/DemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUIApp.swift
+//  DemoSwiftUI
+//
+//  Created by Em bé cute on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
